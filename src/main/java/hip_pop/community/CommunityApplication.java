94e5a8ab2d36f4c2aa7c8c33cee9db84b006095e
HarryBae1011 <06harry@naver.com>
2025-01-14 21:13:06 +0900

    application

---
+++ src/main/java/hip_pop/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package hip_pop.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
+public class CommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApplication.class, args);
+	}
+
+}
