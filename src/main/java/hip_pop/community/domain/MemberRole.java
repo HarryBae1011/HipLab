713964596200370d8fb41a59b64e6cf750af50bd
HarryBae1011 <06harry@naver.com>
2025-01-25 14:50:45 +0900

    Role 추가

---
+++ src/main/java/hip_pop/community/domain/MemberRole.java
@@ -0,0 +1,15 @@
+package hip_pop.community.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum MemberRole {
+    ADMIN("ROLE_ADMIN"),
+    MEMBER("ROLE_MEMBER");
+
+    MemberRole(String value) {
+        this.value = value;
+    }
+
+    private final String value;
+}
