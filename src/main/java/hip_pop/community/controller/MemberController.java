d9c35a051389afec66905225e11bc0229dc2247a
HarryBae1011 <06harry@naver.com>
2025-01-25 15:56:48 +0900

    /members 일괄 매핑

--- src/main/java/hip_pop/community/controller/MemberController.java
+++ src/main/java/hip_pop/community/controller/MemberController.java
@@ -4,27 +4,35 @@ import hip_pop.community.domain.Member;
 import hip_pop.community.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/members/new")
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/new")
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
     }
 
-    @PostMapping("/members/new")
+    @PostMapping("/new")
     public String create(@Valid MemberForm form, BindingResult result) {
 
         if(result.hasErrors()) {
@@ -33,17 +41,22 @@ public class MemberController {
 
         Member member = new Member();
         member.setName(form.getName());
-        member.setPassword(form.getPassword());
+        member.setPassword(passwordEncoder.encode(form.getPassword()));
 
         memberService.join(member);
 
         return "redirect:/";
     }
 
-    @GetMapping("/members")
+    @GetMapping
     public String list(Model model) {
         List<Member> members = memberService.findMembers();
         model.addAttribute("members", members);
         return "members/memberList";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "members/loginForm";
     }
 }
