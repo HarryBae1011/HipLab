b4e64492d783e10623d52a7d3330dc0e9eba8bb9
HarryBae1011 <06harry@naver.com>
2025-05-13 14:48:00 +0900

    postcategory에 label 추가

--- src/main/java/hip_pop/community/domain/enums/PostCategory.java
+++ src/main/java/hip_pop/community/domain/enums/PostCategory.java
@@ -4,5 +4,16 @@ import lombok.Getter;
 
 @Getter
 public enum PostCategory {
-    FREE, REVIEW, MUSIC, ARTIST, NEWS, CONCERT_INFO
+    FREE("자유"),
+    REVIEW("리뷰"),
+    MUSIC("음악"),
+    ARTIST("아티스트"),
+    NEWS("뉴스"),
+    CONCERT_INFO("공연 정보");
+
+    private final String label;
+
+    PostCategory(String label) {
+        this.label = label;
+    }
 }
