13ceda75a26efc919a4032f227b0f90f0d66af56
HarryBae1011 <06harry@naver.com>
2025-05-13 10:15:14 +0900

    댓글 삭제 기능 추가

--- src/main/java/hip_pop/community/web/controller/CommentController.java
+++ src/main/java/hip_pop/community/web/controller/CommentController.java
@@ -1,5 +1,6 @@
 package hip_pop.community.web.controller;
 
+import hip_pop.community.domain.Comment;
 import hip_pop.community.domain.Member;
 import hip_pop.community.domain.Post;
 import hip_pop.community.service.CommentService;
@@ -10,8 +11,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +26,7 @@ public class CommentController {
     private final PostService postService;
     private final MemberService memberService;
 
+    //새로운 댓글 작성
     @PreAuthorize("isAuthenticated()")
     @PostMapping
     public String createComment(@Valid CommentForm form,
@@ -39,4 +39,22 @@ public class CommentController {
 
         return "redirect:/posts/{postId}";
     }
+
+    //댓글 삭제
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/delete/{commentId}")
+    public String deleteComment(@PathVariable("postId") Long postId,
+                                @PathVariable("commentId") Long commentId,
+                                Principal principal) {
+        Comment findComment = commentService.findSingleComment(commentId);
+
+        if (findComment.getCommentMember().getName().equals(principal.getName())) {
+            commentService.deleteComment(commentId);
+        } else {
+            throw new SecurityException("댓글 작성자만 삭제할 수 있습니다.");
+        }
+
+        return "redirect:/posts/{postId}";
+    }
+
 }
